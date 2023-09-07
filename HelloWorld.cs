bb9ab47c4613e059f62e1da4666b5c0a16ea7dd0
sanders93 <sanders93business@gmail.com>
2023-09-06 20:16:20 -0500

    fixes

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -1,8 +1,8 @@
 namespace Test
 {
-	public class HelloWorld
+	class HelloWorld
 	{
-		pubic static void Main()
+		static void Main()
 		{
 			WriteLine("Hello World!");
 			WriteLine("This is Sanders9 World Now!");
